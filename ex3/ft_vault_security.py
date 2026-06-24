a50cf572afabe5ff1e534c05adae85c537bb6260
tmagoudi <tmagoudi@f0r10s16.paris.42.school>
2026-06-24 15:36:52 +0200

    fin exo3

--- ex3/ft_vault_security.py
+++ ex3/ft_vault_security.py
@@ -1,12 +1,25 @@
 import typing
 
 
-def secure_archive(filename :str, action: str, content : str ="") typing.Tuple[bool, str]:
-    pass
+def secure_archive(filename :str, action: str, content : str ="") -> typing.Tuple[bool, str]:
+    try:
+        if action == "r":
+            with open(filename, 'r') as file:
+                return (True, file.read())
+            file.write("newtext")
+        elif action == "w":
+            with open(filename, 'w') as file:
+                file.write(content)
+                return (True, "Content succesfully written to file")
+        else:
+            raise Exception("Invalid Action")
+    except Exception as e:
+        return(False, str(e))
+
 
 def main() -> None:
     print("=== Cyber Archives Security ===")
-    secure_archive("test.txt", "r")
-
+    print(secure_archive("test.txt", "w", "hello"))
+    
 if __name__ == '__main__':
     main()
